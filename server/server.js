8a789958973ec2e13b4c89169ca3602da8e16b71
Amit Dubey <amit.dubey@tothenew.com>
2017-07-31 17:02:27 +0530

    added geolocation

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const port = process.env.PORT || 3000
 
@@ -23,6 +23,10 @@ io.on('connection', (socket) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('this is from server');
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
   socket.on('disconnect', () => {
     console.log('disconnected from client')
