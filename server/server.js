83c75a01588c20f719e458d399c8d6212ebb4082
Amit Dubey <amit.dubey@tothenew.com>
2017-07-31 14:58:45 +0530

    added messages from admin

--- server/server.js
+++ server/server.js
@@ -13,9 +13,21 @@ var io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('new user connected');
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User Joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
