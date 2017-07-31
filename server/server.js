1631935bd4200f66c6fc984421742b0473aea7c8
Amit Dubey <amit.dubey@tothenew.com>
2017-07-31 14:20:13 +0530

    connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000
 
 const publicPath = path.join(__dirname, '../public')
 const app = express();
-app.use(express.static(publicPath));
-app.get('/', (req, res) => {
-  res.send('index.html');
-});
 
-app.listen(port, () => {
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  socket.on('disconnect', () => {
+    console.log('disconnected from client')
+  })
+
+})
+
+app.use(express.static(publicPath));
+
+server.listen(port, () => {
   console.log('server is running at port ' + port)
 })
